342c0bed2cf487777e7f1ccb46af68827d4944e9
Jason Corum <jason.corum@gmail.com>
2018-10-01 12:18:46 -0700

    Uses Nokogiri during ingest to help strip special characters

--- scripts/lib/uncollector.rb
+++ scripts/lib/uncollector.rb
@@ -1,9 +1,11 @@
 require 'rexml/xpath'
 require 'rexml/document'
+require 'nokogiri'
 
 module Uncollector
   def self.uncollect_string(string)
-    doc = REXML::Document.new(string)
+    # Uses Nokogiri to help handle special characters
+    doc = REXML::Document.new(Nokogiri::XML(string).to_s)
     REXML::XPath.match(doc, '/*/*').map(&:to_s)
   end
 end
