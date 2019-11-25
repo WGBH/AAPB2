b4ad6f7d9e80905402b0a629c7566f3bef5c51e8
Jason Corum <jason.corum@gmail.com>
2019-11-25 08:34:20 -0800

    Caches topical_resources

--- app/controllers/topical_resources_controller.rb
+++ app/controllers/topical_resources_controller.rb
@@ -2,7 +2,15 @@ require_relative '../models/special_collection'
 
 class TopicalResourcesController < ApplicationController
   def index
-    @topical_resources = YAML.load_file(Rails.root + 'lib/resource_sets/topical_resources.yml')
+    @topical_resources = all_topical_resources
     @page_title = 'Topical Resources'
+  end
+
+  private
+
+  def all_topical_resources
+    Rails.cache.fetch('canonical_urls') do
+      YAML.load_file(Rails.root + 'lib/resource_sets/topical_resources.yml')
+    end
   end
 end
